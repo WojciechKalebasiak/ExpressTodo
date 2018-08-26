193fa25654af774806b6c92938fcadf8d57cb4d9
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-08-26 23:54:07 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectID;
 
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/User');
@@ -22,7 +23,23 @@ app.get('/todos', (req, res) => {
     }, e => {
         res.status(400).send(e);
     });
-})
+});
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid id');
+    }
+    ToDo.findById(id).then(todo => {
+        if (!todo) {
+            return res.status(404).send('User not found');
+        }
+        else {
+            res.status(200).send({ todo });
+        }
+    }).catch(e => {
+        res.status(404).send('Invalid id');
+    });
+});
 app.listen(3000, () => {
     console.log('Starting at http://localhost:3000');
 });
